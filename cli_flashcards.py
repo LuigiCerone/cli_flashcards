592b8b2a5d8c0bb8afb7723c944b84198cab40b2
Luigi Cerone <luigicerone96@gmail.com>
2022-09-22 23:17:19 +0200

    Add first step in restore old state logic

--- cli_flashcards.py
+++ cli_flashcards.py
@@ -2,11 +2,11 @@ import argparse
 import re
 import pandas as pd
 import warnings
-import pathlib
+from pathlib import Path
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-def load_questions(file_name: str, known_dir: pathlib.Path) -> pd.DataFrame:
+def load_questions(file_name: str, known_dir: Path) -> pd.DataFrame:
     # TODO Store progress in a file and try to read them back according to file name.
     if not known_dir.is_dir():
         known_dir.mkdir(parents=True, exist_ok=True)
@@ -14,10 +14,18 @@ def load_questions(file_name: str, known_dir: pathlib.Path) -> pd.DataFrame:
     print(f"Reading questions from file: {file_name}")
     df = pd.read_csv(filepath_or_buffer=file_name, sep='\t', names=['Question', 'Answer'], lineterminator='\n')
     print(f"Succesfully loaded {len(df)} questions")
+   
+    old_state = Path.joinpath(known_dir, Path(file_name).with_suffix('.csv'))
+    print(f"Searching for: {old_state}")
+    if old_state.is_file():
+        # TODO Check length
+        old_indexes_df = pd.read_csv(filepath_or_buffer=old_state)
+        print(old_indexes_df.head())
+
     return df
 
 def read_and_validate_user_input():
-    i = input('Input (y/n/s/q):  ').lower()
+    i = input('Input (y/n/s/q):\t').lower()
 
     if len(i) > 1:
         raise Exception('Wrong input length provided. Please read the doc.')
@@ -27,7 +35,11 @@ def read_and_validate_user_input():
     
     return i
 
-def main(file_name: str, known_dir: pathlib.Path):
+def store_known_questions(known_df: pd.DataFrame, file_name: str, known_dir: Path):
+    dest = Path.joinpath(known_dir, Path(file_name))
+    known_df.to_csv(dest, columns=[], header=False)
+
+def main(file_name: str, known_dir: Path):
     question_df = load_questions(file_name=file_name, known_dir=known_dir)
     known_df = pd.DataFrame(columns=question_df.columns)
 
@@ -47,7 +59,7 @@ def main(file_name: str, known_dir: pathlib.Path):
             print('Exiting')
             break
         elif i == 'y':
-            known_df = known_df.append(random_row, ignore_index=True)
+            known_df = known_df.append(random_row, ignore_index=False)
             question_df.drop(random_row.index, inplace=True)
         elif i == 'n':
             pass
@@ -57,12 +69,13 @@ def main(file_name: str, known_dir: pathlib.Path):
             print(f"Unknown input provided: {i}. Please read the doc")
 
     print(f"\nQuestions recap: \n\t Known: {known_df.shape[0]} \n\t Still to learn: {question_df.shape[0]}")
+    store_known_questions(known_df, file_name, known_dir)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('file', help='File path to flashcards file', type=argparse.FileType('r', encoding='UTF-8'))
-    parser.add_argument('--store', help='File path to store known answers dir', type=pathlib.Path, default='./store_known/')
+    parser.add_argument('--store', help='File path to store known answers dir', type=Path, default='./store_known/')
 
     args = parser.parse_args()
     main(args.file.name, args.store)
