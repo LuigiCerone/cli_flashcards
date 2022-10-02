cb05937edb716a9e518b19bb056eb13de301c9f8
Luigi Cerone <luigicerone96@gmail.com>
2022-10-02 16:10:32 +0200

    Fix demo file and fix bug on known questions storage

--- cli_flashcards.py
+++ cli_flashcards.py
@@ -12,16 +12,25 @@ def load_questions(file_name: Path, known_dir: Path) -> pd.DataFrame:
     print(f"Reading questions from file: {file_name}")
     df = pd.read_csv(filepath_or_buffer=file_name, sep='\t', names=['Question', 'Answer'], lineterminator='\n')
    
+    old_state_df = get_old_state(file_name=file_name, known_dir=known_dir)
+
+    if not old_state_df.empty:
+        known_df = df[df.index.isin(old_state_df['Known_id'])]
+        df.drop(old_state_df['Known_id'], axis=0, inplace=True)
+        print(f"Succesfully loaded {len(df)} questions, restored {len(old_state_df)} known questions from previous runs.\n")
+    else:
+        known_df = pd.DataFrame(columns=df.columns)
+        print(f"Succesfully loaded {len(df)} questions, restored 0 known questions from previous runs.\n")
+    return df, known_df
+
+def get_old_state(file_name: Path, known_dir: Path):
     old_state = Path.joinpath(known_dir, file_name.with_suffix('.csv').name)
     print(f"Searching for: {old_state}")
-    if old_state.is_file() and old_state.stat().st_size != 0:
-        old_indexes_df = pd.read_csv(filepath_or_buffer=old_state, header=None, names=['Known_id'])
-        df.drop(old_indexes_df['Known_id'], axis=0, inplace=True)
-        print(f"Succesfully loaded {len(df)} questions, restored {len(old_indexes_df)} known questions from previous runs.\n")
-    else:
-        print(f"Succesfully loaded {len(df)} questions, restored 0 known questions from previous runs.\n")
 
-    return df
+    if old_state.is_file() and old_state.stat().st_size != 0:
+        return pd.read_csv(filepath_or_buffer=old_state, header=None, names=['Known_id'])
+    else:
+        return pd.DataFrame()
 
 def read_and_validate_user_input():
     i = input('Input (y/n/s/q):\t').lower()
@@ -39,17 +48,16 @@ def store_known_questions(known_df: pd.DataFrame, file_name: Path, known_dir: Pa
     known_df.to_csv(dest, columns=[], header=False)
 
 def main(file_name: Path, known_dir: Path):
-    question_df = load_questions(file_name=file_name, known_dir=known_dir)
-    known_df = pd.DataFrame(columns=question_df.columns)
+    questions_df, known_df = load_questions(file_name=file_name, known_dir=known_dir)
 
     print('Type "y" to mark as known, "n" to mark as to study, "s" to show the answer or "q" to exit.')
 
     while True:
-        if len(question_df) <= 0:
+        if len(questions_df) <= 0:
             print("Congratulations! Deck mastered!")
             break
 
-        random_row = question_df.sample(n=1)
+        random_row = questions_df.sample(n=1)
         print(f"\n*** New question: \n\t{random_row['Question'].values[0]}\n")
 
         try:
@@ -63,7 +71,7 @@ def main(file_name: Path, known_dir: Path):
             break
         elif i == 'y':
             known_df = known_df.append(random_row, ignore_index=False)
-            question_df.drop(random_row.index, inplace=True)
+            questions_df.drop(random_row.index, inplace=True)
         elif i == 'n':
             pass
         elif i == 's':
@@ -71,9 +79,8 @@ def main(file_name: Path, known_dir: Path):
         else:
             print(f"Unknown input provided: {i}. Please read the doc")
 
-    print(f"\nQuestions recap: \n\t Known: {known_df.shape[0]} \n\t Still to learn: {question_df.shape[0]}")
+    print(f"\nQuestions recap: \n\t Known: {known_df.shape[0]} \n\t Still to learn: {questions_df.shape[0]}")
     store_known_questions(known_df, file_name, known_dir)
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
